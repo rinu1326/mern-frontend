4e9f1e01374aed17f01b3276a9163f035e64af49
rinu1326 <nifu4040@gmail.com>
2025-05-16 13:53:57 +0530

    Fix api import casing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../utils/api";
+import API from "./utils/api";
 import "./AuthForm.css";
 
 const Login = () => {
